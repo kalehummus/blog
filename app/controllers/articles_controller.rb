1ae7ba5f4c31e501727a72fe7f61172a97497a21
kalehummus <nsubishah@gmail.com>
2018-08-17 21:11:58 -0500

    unredundantized

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,26 +1,28 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
   def index
     @articles = Article.all
-
   end
+
   def new
     @article = Article.new
   end
 
   def edit
-    @article = Article.find(params[:id])
+    set_article
   end
 
   def destroy
-    @article = Article.find(params[:id])
+    set_article
     @article.destroy
     flash[:notice] = "Article was deleted gr8"
     redirect_to articles_path(@article)
   end
 
   def update
-    @article = Article.find(params[:id])
+    set_article
     if @article.update(article_params)
       flash[:notice] = "Article was updated gr8" #this notice needs to be displayed in a view
       redirect_to article_path(@article)
@@ -42,12 +44,16 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+    set_article
   end
+
   private
     def article_params
       params.require(:article).permit(:title, :description)
     end
 
+    def set_article
+      @article = Article.find(params[:id])
+    end
 
 end
